8d06eb2c96a97f7b3a6674c082ddaa595b90b3fe
hjh <sniezny@naver.com>
2022-03-22 12:28:31 +0900

    로그인 추가

--- src/main/java/com/itwillbs/controller/MemberController.java
+++ src/main/java/com/itwillbs/controller/MemberController.java
@@ -25,29 +25,47 @@ public class MemberController {
 	
 	
 	
+	
+	
+	
+	
+	
+	@RequestMapping(value = "/member/login", method = RequestMethod.GET)
+	public String login() {
+		return "member/login";
+	}
+	
+	@RequestMapping(value = "/member/loginPro", method = RequestMethod.POST)
+	public String loginPro(MemberDTO memberDTO, HttpSession session) {
+		
+		MemberDTO ckDTO = memberService.userCheck(memberDTO);
+		
+		if (ckDTO!=null) {
+			session.setAttribute("userid", memberDTO.getUserid());
+			return "redirect:/main/main";
+			
+		} else {
+			return "member/msg";
+		}
+	}
+	
 	@RequestMapping(value = "/member/update", method = RequestMethod.GET)
 	public String update(HttpSession session, Model model) {
 		
 		System.out.println("Test");
-		//String id = (String) session.getAttribute("id");
-		String id = "asdasd";
-		MemberDTO memberDTO = memberService.getMember(id);
+		String userid = (String) session.getAttribute("userid");
+		MemberDTO memberDTO = memberService.getMember(userid);
 		model.addAttribute("memberDTO", memberDTO);
 		
 		return "member/update";
 	}
 	
 	@RequestMapping(value = "/member/updatePro", method = RequestMethod.POST)
-	public String updatePro(MemberDTO memberDTO, HttpSession session) {
-		
-		
+	public String updatePro(MemberDTO memberDTO) {
 		System.out.println("객체 : " + memberDTO);
 		System.out.println("아이디 : " + memberDTO.getUserid());
 		System.out.println("pw : " + memberDTO.getPassword());
 		System.out.println("이름 : " + memberDTO.getName());
-		
-		
-		System.out.println("MemberController updatePro()");
 		MemberDTO ckDTO = memberService.userCheck(memberDTO);
 
 		if (ckDTO!=null) {
